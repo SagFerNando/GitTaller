7162db84e87b4390ba6bd483bc873411c8b76536
SagFerNando <sa464030@uaeh.edu.mx>
2024-02-16 13:39:06 -0600

    add fers.js

---
+++ javascript/fers.js
@@ -0,0 +1,14 @@
+console.log("Esto es un mensaje en el log");
+// error
+console.error("Esto es un error");
+// Añadir información
+console.info("Añade información a la consola");
+// warning
+console.warn("Envía un aviso");
+
+const gente = [
+	{nombre: 'Diego', edad: 30},
+  {nombre: 'Toni', edad: 10},
+  {nombre: 'Johana', edad: 20},
+];
+console.table(gente);
